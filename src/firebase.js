b89bf9920a8901d2094ebe0b0bc06d083434e460
Krishnendu Dasgupta <72221973+KrishnenduDG@users.noreply.github.com>
2024-04-19 01:39:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import {getStorage} from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_w_cG9Co7J8s8hGWkh6xyuMdpXe3POCc",
-  authDomain: "pg-huntz-main.firebaseapp.com",
-  projectId: "pg-huntz-main",
-  storageBucket: "pg-huntz-main.appspot.com",
-  messagingSenderId: "297626407000",
-  appId: "1:297626407000:web:e18de7b9dd8f1cd3a9fa2d",
-  measurementId: "G-YTX8EF3RCG",
+  apiKey: "AIzaSyADEpze-xc1NUqXIncIJOItLqkHiD2gCkg",
+  authDomain: "pg-huntz-8043b.firebaseapp.com",
+  projectId: "pg-huntz-8043b",
+  storageBucket: "pg-huntz-8043b.appspot.com",
+  messagingSenderId: "1034810940190",
+  appId: "1:1034810940190:web:26fb0099fd340fb166070f",
+  measurementId: "G-85EYVW01GQ"
 };
 
 const app = initializeApp(firebaseConfig);
